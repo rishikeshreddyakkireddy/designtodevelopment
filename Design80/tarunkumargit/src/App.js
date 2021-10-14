5bfba628f59474490f6a4df07a2539cadb279858
Tarun kumar <tarunkumar7973@gmail.com>
2021-10-14 18:10:21 +0530

    list

--- Design80/tarunkumargit/src/App.js
+++ Design80/tarunkumargit/src/App.js
@@ -3,11 +3,14 @@ import './App.scss';
 
 // Components
 import Header from './components/Header/Header';
+import List from './components/List/List';
 
 const App = () => {
   return (
     <section className="landing__page">
+      <div className="landing__page__bg__circle"></div>
       <Header />
+      <List />
     </section>
   );
 };
